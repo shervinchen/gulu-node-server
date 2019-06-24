21cc7aad7859e0399bc165627fb666ea08bd1b57
csdoker <csd758371536@qq.com>
2019-06-24 09:34:13 +0800

    init

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express')
+const multer = require('multer')
+const upload = multer({dest: 'uploads/'})
+const cors = require('cors')
+
+const app = express()
+
+
+app.get('/', (req, res) => {
+    res.send('hello nodejs')
+})
+
+app.options('/upload', cors())
+app.post('/upload', cors(), upload.single('file'), (req, res) => {
+    res.send(req.file.filename)
+})
+
+// app.get('/preview/:key', (req, res) => {
+//     res.sendFile(`uploads/${req.params.key}`, {
+//         root: __dirname,
+//         headers: {
+//             'Content-Type': 'image/jpeg'
+//         }
+//     }, (error) => {
+//         res.status(404).send('file not found')
+//     })
+// })
+
+app.get('/preview/:key', cors(), function (req, res, next) {
+    var options = {
+        root: __dirname,
+        headers: {
+            'Content-Type': 'image/jpeg'
+        }
+    }
+    res.sendFile(`uploads/${req.params.key}`, options, function (err) {
+        if (err) {
+            res.status(404).send('file not found')
+            next(err)
+        } else {
+            console.log('Sent:', req.params.key)
+        }
+    })
+})
+
+app.listen(3000)
